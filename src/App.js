e335c88f01e8cf2e7bca5762f8063a5c2443eb8f
NIRUPASHREE <snirupasree03@gmail.com>
2021-05-10 16:53:53 +0530

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavigationBar from './NavigationBar /NavigationBar'
 import Fitness from './Fitness/Fitness';
 
 const App = (props) => {
